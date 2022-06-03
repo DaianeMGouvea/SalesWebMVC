1b06a0f85a337573ae49f85049d9b7529b8973fd
= <=>
2022-06-03 16:43:03 -0300

    SellerService

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,27 +1,46 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 string mySqlConnection = builder.Configuration.GetConnectionString("SalesWebMVCContext");
 
-builder.Services.AddDbContextPool<SalesWebMVCContext>(options =>
-                options.UseMySql(mySqlConnection,
+builder.Services.AddDbContextPool<SalesWebMVCContext>(options => options.UseMySql(mySqlConnection,
                       ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddScoped<SeedingService>();
+builder.Services.AddTransient<SellerService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
+
+
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    SeedDatabase();
+}
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
+}
+
 
 
 app.UseHttpsRedirection();
