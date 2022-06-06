9c8614dc2b7ac02db2b6a9d7a2019dea032cefea
= <=>
2022-06-06 12:43:03 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -8,14 +8,16 @@
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
-            
+
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department = null)
         {
             Id = id;
             Name = name;
@@ -23,6 +25,7 @@
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
+   
         }
 
         public void AddSales(SalesRecord sr)
